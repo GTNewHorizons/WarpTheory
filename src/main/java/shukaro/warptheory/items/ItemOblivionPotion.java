1b08e1ac974d651b3dbd210cd816d3beeeb651b8
koolkrafter5 <creepersrgreen@gmail.com>
2026-05-05 22:43:40 -0400

    New Item: Potion of Blissful Oblivion (#48)
    Co-authored-by: Martin Robertz <dream-master@gmx.net>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: GitHub GTNH Actions <2970688+Dream-Master@users.noreply.github.com>

---
+++ src/main/java/shukaro/warptheory/items/ItemOblivionPotion.java
@@ -0,0 +1,94 @@
+package shukaro.warptheory.items;
+
+import java.util.List;
+import java.util.Locale;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
+import net.minecraft.item.EnumAction;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ChatComponentTranslation;
+import net.minecraft.util.StatCollector;
+import net.minecraft.world.World;
+
+import cpw.mods.fml.common.gameevent.PlayerEvent;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import shukaro.warptheory.handlers.WarpHandler;
+import shukaro.warptheory.util.Constants;
+import thaumcraft.api.aspects.Aspect;
+import thaumcraft.api.research.ResearchCategories;
+import thaumcraft.api.research.ResearchCategoryList;
+import thaumcraft.api.research.ResearchItem;
+import thaumcraft.common.Thaumcraft;
+
+public class ItemOblivionPotion extends Item {
+
+    public ItemOblivionPotion() {
+        this.setHasSubtypes(false);
+        this.setMaxStackSize(1);
+        this.setUnlocalizedName(Constants.ITEM_POTION);
+        this.setTextureName(Constants.modID.toLowerCase(Locale.ENGLISH) + ":itemOblivionPotion");
+    }
+
+    @Override
+    public ItemStack onItemRightClick(ItemStack stack, World world, EntityPlayer player) {
+        warnPlayer(player);
+        player.setItemInUse(stack, this.getMaxItemUseDuration(stack));
+        return super.onItemRightClick(stack, world, player);
+    }
+
+    private void warnPlayer(EntityPlayer player) {
+        if (player.worldObj.isRemote) return;
+        player.addChatMessage(new ChatComponentTranslation("chat.warptheory.potionwarning.1"));
+        player.addChatMessage(new ChatComponentTranslation("chat.warptheory.potionwarning.2"));
+    }
+
+    @Override
+    public ItemStack onEaten(ItemStack stack, World world, EntityPlayer player) {
+        clearAllKnowledgeAndWarp(player);
+        if (!player.capabilities.isCreativeMode) {
+            if (stack.stackSize <= 1) {
+                return new ItemStack(Items.glass_bottle);
+            }
+            stack.stackSize--;
+            player.inventory.addItemStackToInventory(new ItemStack(Items.glass_bottle));
+        }
+        return super.onEaten(stack, world, player);
+    }
+
+    private void clearAllKnowledgeAndWarp(EntityPlayer player) {
+        if (player.worldObj.isRemote) return;
+        String name = player.getCommandSenderName();
+        WarpHandler.Knowledge.wipePlayerKnowledge(name);
+        for (ResearchCategoryList category : ResearchCategories.researchCategories.values()) {
+            for (ResearchItem research : category.research.values()) {
+                if (research.isAutoUnlock()) {
+                    Thaumcraft.proxy.getResearchManager().completeResearch(player, research.key);
+                }
+            }
+        }
+        // Add the initial 15-19 primal knowledge points given when a player joins for the first time
+        for (Aspect aspect : Aspect.getPrimalAspects()) {
+            WarpHandler.Knowledge.setAspectPool(name, aspect, (short) (15 + player.worldObj.rand.nextInt(5)));
+        }
+        // Syncs everything to the player. Kind of hacky to manually send an event like this, but it seems to work.
+        Thaumcraft.instance.networkEventHandler.playerLoggedInEvent(new PlayerEvent.PlayerLoggedInEvent(player));
+    }
+
+    @Override
+    public int getMaxItemUseDuration(ItemStack stack) {
+        return 200;
+    }
+
+    @SideOnly(Side.CLIENT)
+    public void addInformation(ItemStack item, EntityPlayer player, List<String> tooltip, boolean advanced) {
+        tooltip.add(StatCollector.translateToLocal("tooltip.warptheory.oblivionpotion"));
+    }
+
+    @Override
+    public EnumAction getItemUseAction(ItemStack stack) {
+        return EnumAction.drink;
+    }
+}
--- src/main/java/shukaro/warptheory/research/WarpResearch.java
+++ src/main/java/shukaro/warptheory/research/WarpResearch.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.StatCollector;
 
 import shukaro.warptheory.items.WarpItems;
 import shukaro.warptheory.recipe.WarpRecipes;
@@ -15,13 +14,14 @@ import thaumcraft.api.aspects.Aspect;
 import thaumcraft.api.aspects.AspectList;
 import thaumcraft.api.crafting.CrucibleRecipe;
 import thaumcraft.api.crafting.InfusionRecipe;
+import thaumcraft.api.crafting.ShapedArcaneRecipe;
 import thaumcraft.api.crafting.ShapelessArcaneRecipe;
 import thaumcraft.api.research.ResearchCategories;
 import thaumcraft.api.research.ResearchPage;
 
 public class WarpResearch {
 
-    public static HashMap<String, Object> recipes = new HashMap();
+    public static HashMap<String, Object> recipes = new HashMap<>();
 
     public static void init() {
         ResearchCategories.registerCategory(
@@ -41,11 +41,10 @@ public class WarpResearch {
                 -1,
                 -2,
                 2,
-                new ItemStack(WarpItems.itemCleanserMinor)).setPages(
-                        new ResearchPage[] {
-                                new ResearchPage(
-                                        StatCollector.translateToLocal("research.warptheory.warpcleanserminor")),
-                                new ResearchPage((InfusionRecipe) recipes.get("PureTearMinor")) })
+                new ItemStack(WarpItems.itemCleanserMinor))
+                        .setPages(
+                                new ResearchPage("research.warptheory.warpcleanserminor"),
+                                new ResearchPage((InfusionRecipe) recipes.get("PureTearMinor")))
                         .setParents("ELDRITCHMAJOR", Constants.ITEM_LITMUS, "FAKEELDMAJOR").setHidden()
                         .setItemTriggers(ItemApi.getItem("itemSanitySoap", 0)).registerResearchItem();
 
@@ -55,10 +54,10 @@ public class WarpResearch {
                 -3,
                 -2,
                 2,
-                new ItemStack(WarpItems.itemCleanser)).setPages(
-                        new ResearchPage[] {
-                                new ResearchPage(StatCollector.translateToLocal("research.warptheory.warpcleanser")),
-                                new ResearchPage((InfusionRecipe) recipes.get("PureTear")) })
+                new ItemStack(WarpItems.itemCleanser))
+                        .setPages(
+                                new ResearchPage("research.warptheory.warpcleanser"),
+                                new ResearchPage((InfusionRecipe) recipes.get("PureTear")))
                         .setParents("ELDRITCHMAJOR", Constants.ITEM_LITMUS, "FAKEELDMAJOR").setHidden()
                         .setItemTriggers(ItemApi.getItem("itemSanitySoap", 0)).registerResearchItem();
 
@@ -70,25 +69,24 @@ public class WarpResearch {
                 3,
                 new ItemStack(WarpItems.itemAmulet))
                         .setPages(
-                                new ResearchPage[] {
-                                        new ResearchPage(StatCollector.translateToLocal("research.warptheory.amulet")),
-                                        new ResearchPage((InfusionRecipe) recipes.get("PureAmulet")) })
+                                new ResearchPage("research.warptheory.amulet"),
+                                new ResearchPage((InfusionRecipe) recipes.get("PureAmulet")))
                         .setParents(Constants.ITEM_WARPCLEANSER).setConcealed().registerResearchItem();
 
         new WTResearchItem(
                 Constants.ITEM_PORTABLE_SHOWER,
                 new AspectList().add(Aspect.ELDRITCH, 6).add(Aspect.EXCHANGE, 3).add(Aspect.MAGIC, 3),
-                5,
-                5,
+                -2,
+                -4,
                 3,
-                new ItemStack(WarpItems.itemWarpWardAmulet)).setPages(
-                        new ResearchPage[] {
-                                new ResearchPage(StatCollector.translateToLocal("research.warptheory.portableshower")),
-                                new ResearchPage((InfusionRecipe) recipes.get("WarpWardAmulet")) })
+                new ItemStack(WarpItems.itemWarpWardAmulet))
+                        .setPages(
+                                new ResearchPage("research.warptheory.portableshower"),
+                                new ResearchPage((InfusionRecipe) recipes.get("WarpWardAmulet")))
                         .setParents(Constants.ITEM_PURE_TALISMAN).setConcealed().registerResearchItem();
 
         ResearchPage[] somethingPages = new ResearchPage[WarpRecipes.meats.size() + 1];
-        somethingPages[0] = new ResearchPage(StatCollector.translateToLocal("research.warptheory.warpsomething"));
+        somethingPages[0] = new ResearchPage("research.warptheory.warpsomething");
         int i = 1;
         for (ItemStack meat : WarpRecipes.meats) {
             somethingPages[i] = (new ResearchPage((CrucibleRecipe) recipes.get("WarpChunk" + meat)));
@@ -113,10 +111,22 @@ public class WarpResearch {
                 2,
                 new ItemStack(WarpItems.itemPaper))
                         .setPages(
-                                new ResearchPage[] {
-                                        new ResearchPage(StatCollector.translateToLocal("research.warptheory.paper")),
-                                        new ResearchPage((ShapelessArcaneRecipe) recipes.get("Litmus")) })
+                                new ResearchPage("research.warptheory.paper"),
+                                new ResearchPage((ShapelessArcaneRecipe) recipes.get("Litmus")))
                         .setConcealed().setParents("FAKESOAP")
                         .setAspectTriggers(Aspect.ELDRITCH, Aspect.MAGIC, Aspect.SENSES).registerResearchItem();
+
+        new WTResearchItem(
+                Constants.ITEM_POTION,
+                new AspectList().add(Aspect.MIND, 3).add(Aspect.HEAL, 3),
+                -2,
+                -1,
+                0,
+                new ItemStack(WarpItems.itemOblivionPotion))
+                        .setPages(
+                                new ResearchPage("research.warptheory.oblivionpotion.1"),
+                                new ResearchPage((ShapedArcaneRecipe) recipes.get("Potion")),
+                                new ResearchPage("research.warptheory.oblivionpotion.2"))
+                        .setAutoUnlock().setSpecial().registerResearchItem();
     }
 }
